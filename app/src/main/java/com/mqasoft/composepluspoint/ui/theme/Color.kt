dd2f20a9998b949cf57fa388bb494488622df6ed
Abdul Moeed <abdul.moeed@kentkart.com>
2024-03-14 14:46:05 +0300

    [0.0.0] continuing on Nfc screen

--- app/src/main/java/com/mqasoft/composepluspoint/ui/theme/Color.kt
+++ app/src/main/java/com/mqasoft/composepluspoint/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val DarkBluePlusPoint = Color(0xFF2D334F)
+val GreyPlusPoint = Color(0xFFEEF3F6)
+val BluePlusPoint = Color(0XFF4773C8)
+val GreenPlusPoint = Color(0xFF51A64A)
